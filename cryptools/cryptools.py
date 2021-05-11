7274d0f691517d8d449e12601f45896957091110
Guillaume Marcel <guillaume.k.marcel@gmail.com>
2021-05-11 17:22:53 +0200

    added + tested CaesarCipher

--- cryptools/cryptools.py
+++ cryptools/cryptools.py
@@ -22,3 +22,30 @@ class AtbashCipher(Cipher):
 
     def decode(self, ciphertext: str) -> str:
         return str.translate(ciphertext.upper(), self._table)
+
+
+class CaesarCipher(Cipher):
+
+    def __init__(self, key: int):
+        super().__init__()
+        self.key: int = key
+
+    def _process(self, text: str, key: int) -> str:
+        processed_text: list[str] = []
+
+        for char in text.upper():
+            if char.isalpha():
+                index: int = ord(char) + key % 26
+                if index > ord('Z'):
+                    index = index - 26
+                processed_text.append(chr(index))
+            else:
+                processed_text.append(char)
+
+        return ''.join(processed_text)
+
+    def encode(self, plaintext: str) -> str:
+        return self._process(plaintext, self.key) if self.key != 0 else plaintext.upper()
+
+    def decode(self, ciphertext: str) -> str:
+        return self._process(ciphertext, -1 * self.key)
--- tests/tests.py
+++ tests/tests.py
@@ -3,6 +3,53 @@ import unittest
 import cryptools.cryptools as cryptools
 
 
+class TestCaesar(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.plaintext_only_letters = 'This message shall remain private'
+        self.ciphertexts_only_letters = {
+            0: 'THIS MESSAGE SHALL REMAIN PRIVATE',
+            1: 'UIJT NFTTBHF TIBMM SFNBJO QSJWBUF',
+            2: 'VJKU OGUUCIG UJCNN TGOCKP RTKXCVG',
+            3: 'WKLV PHVVDJH VKDOO UHPDLQ SULYDWH',
+            4: 'XLMW QIWWEKI WLEPP VIQEMR TVMZEXI',
+            5: 'YMNX RJXXFLJ XMFQQ WJRFNS UWNAFYJ',
+            6: 'ZNOY SKYYGMK YNGRR XKSGOT VXOBGZK',
+            7: 'AOPZ TLZZHNL ZOHSS YLTHPU WYPCHAL',
+            8: 'BPQA UMAAIOM APITT ZMUIQV XZQDIBM',
+            9: 'CQRB VNBBJPN BQJUU ANVJRW YAREJCN',
+            10: 'DRSC WOCCKQO CRKVV BOWKSX ZBSFKDO',
+            11: 'ESTD XPDDLRP DSLWW CPXLTY ACTGLEP',
+            12: 'FTUE YQEEMSQ ETMXX DQYMUZ BDUHMFQ',
+            13: 'GUVF ZRFFNTR FUNYY ERZNVA CEVINGR',
+            14: 'HVWG ASGGOUS GVOZZ FSAOWB DFWJOHS',
+            15: 'IWXH BTHHPVT HWPAA GTBPXC EGXKPIT',
+            16: 'JXYI CUIIQWU IXQBB HUCQYD FHYLQJU',
+            17: 'KYZJ DVJJRXV JYRCC IVDRZE GIZMRKV',
+            18: 'LZAK EWKKSYW KZSDD JWESAF HJANSLW',
+            19: 'MABL FXLLTZX LATEE KXFTBG IKBOTMX',
+            20: 'NBCM GYMMUAY MBUFF LYGUCH JLCPUNY',
+            21: 'OCDN HZNNVBZ NCVGG MZHVDI KMDQVOZ',
+            22: 'PDEO IAOOWCA ODWHH NAIWEJ LNERWPA',
+            23: 'QEFP JBPPXDB PEXII OBJXFK MOFSXQB',
+            24: 'RFGQ KCQQYEC QFYJJ PCKYGL NPGTYRC',
+            25: 'SGHR LDRRZFD RGZKK QDLZHM OQHUZSD',
+            26: 'THIS MESSAGE SHALL REMAIN PRIVATE',
+        }
+
+    def test_encode(self) -> None:
+        for i in range(27):
+            self.assertEqual(cryptools.CaesarCipher(i).encode(
+                self.plaintext_only_letters), self.ciphertexts_only_letters[i]
+            )
+
+    def test_decode(self) -> None:
+        for i in range(27):
+            self.assertEqual(cryptools.CaesarCipher(i).decode(
+                self.ciphertexts_only_letters[i]), self.plaintext_only_letters.upper()
+            )
+
+
 class TestAtbash(unittest.TestCase):
 
     def test_encode_only_letters(self) -> None:
