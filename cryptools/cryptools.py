1efb5980ba95eee175771c5df408bcb3595315e9
Guillaume Marcel <guillaume.k.marcel@gmail.com>
2021-05-11 16:25:17 +0200

    added + tested Atbash cipher

--- cryptools/cryptools.py
+++ cryptools/cryptools.py
@@ -1,3 +1,6 @@
+import string
+
+
 class Cipher:
     def __init__(self):
         super().__init__()
@@ -7,3 +10,15 @@ class Cipher:
 
     def decode(self, ciphertext: str) -> str:
         raise NotImplementedError
+
+
+class AtbashCipher(Cipher):
+    def __init__(self):
+        super().__init__()
+        self._table = str.maketrans(string.ascii_uppercase, string.ascii_uppercase[::-1])
+
+    def encode(self, plaintext: str) -> str:
+        return str.translate(plaintext.upper(), self._table)
+
+    def decode(self, ciphertext: str) -> str:
+        return str.translate(ciphertext.upper(), self._table)
--- tests/tests.py
+++ tests/tests.py
@@ -15,6 +15,25 @@ class TestBase(unittest.TestCase):
         self.keys.clear()
 
 
+class TestAtbash(TestBase):
+
+    def test_encode_only_letters(self) -> None:
+        plaintext, ciphertext = 'This message shall remain private', 'GSRH NVHHZTV HSZOO IVNZRM KIREZGV'
+        self.assertEqual(cryptools.AtbashCipher().encode(plaintext), ciphertext)
+
+    def test_decode_only_letters(self) -> None:
+        plaintext, ciphertext = 'THIS MESSAGE SHALL REMAIN PRIVATE', 'gsrh nvhhztv hszoo ivnzrm kirezgv'
+        self.assertEqual(cryptools.AtbashCipher().decode(ciphertext), plaintext)
+
+    def test_encode_any_character(self) -> None:
+        plaintext, ciphertext = '#>This messag@ shall rema5!in private!', '#>GSRH NVHHZT@ HSZOO IVNZ5!RM KIREZGV!'
+        self.assertEqual(cryptools.AtbashCipher().encode(plaintext), ciphertext)
+
+    def test_decode_any_character(self) -> None:
+        plaintext, ciphertext = '#>THIS MESSAG@ SHALL REMA5!IN PRIVATE!', '#>gsrh nvhhzt@ hszoo ivnz5!rm kirezgv!'
+        self.assertEqual(cryptools.AtbashCipher().decode(ciphertext), plaintext)
+
+
 class TestCipher(TestBase):
 
     def test_constructor(self) -> None:
@@ -24,6 +43,6 @@ class TestCipher(TestBase):
         with self.assertRaises(NotImplementedError):
             cryptools.Cipher().encode('plaintext')
 
-    def test_encode(self) -> None:
+    def test_decode(self) -> None:
         with self.assertRaises(NotImplementedError):
             cryptools.Cipher().decode('ciphertext')
